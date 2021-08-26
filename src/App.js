ec5c78592312cdae095f880a7eff23fd65eb38ed
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-08-26 16:35:49 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Dictionary
+      </header>
+    </div>
+  );
+}
+
