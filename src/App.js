db4b71548c00147cf8a6a200aa9c7554f8a1f439
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-08-29 11:49:33 +0200

    added Dictionary component, basic set up of search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         Dictionary
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+        coded by me 
+        </small>
+      </footer>
+      </div>
     </div>
   );
 }
