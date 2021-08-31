0a80b97ee1e1fb37e6349c0157a13bcac83c0113
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-08-31 17:25:02 +0200

    added phonetic's component with audio and text

---
+++ src/Phonetic.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Phonetic(props){
+    console.log(props.phonetic);
+    return (
+        <div className="Phonetic">
+           <a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a> {""}
+           [{props.phonetic.text}]
+        </div>);
+}
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props){
-    console.log(props.results);
     if (props.results){
         return (
             <div className="Results">
                 <h1>{props.results.word}</h1>
+                {props.results.phonetics.map(function(phonetic, index) {
+                    return (
+                        <div key={index}>
+                            <Phonetic phonetic={phonetic} />
+                         </div>
+                    )
+                })}
                  {props.results.meanings.map(function(meaning, index) {
                      // return meaning.definitions[0].definition;
                      return (
@@ -16,7 +23,7 @@ export default function Results(props){
                      )
                  })}
             </div>
-        )
+        );
     } else{
         return null;
     }
