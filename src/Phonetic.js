d64346b346359fccc2504e9d519dcd557a344eae
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-09-01 19:04:31 +0200

    added icon and styling for phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props){
     console.log(props.phonetic);
     return (
         <div className="Phonetic">
-           <a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a> {""}
+           <a href={props.phonetic.audio} target="_blank" rel="noreferrer"><i class="far fa-play-circle"></i></a> {""}
            [{props.phonetic.text}]
         </div>);
 }
